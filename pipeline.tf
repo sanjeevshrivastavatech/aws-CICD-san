8fb51f00809122020e0cd939ea4ec365f54dac17
sanjeevshrivastavatech <76872263+sanjeevshrivastavatech@users.noreply.github.com>
2021-03-28 18:02:52 +0530

    initial code

---
+++ pipeline.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7258f47950286825f3c0a21ddfbc1a3518fdf879c04ff36bedf8a0dec298fbcf
+size 2834
---
+++ s3.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0e08d7ad178155af94a6fe653576a8fcd587edf1071edc20a6bdaf740158008
+size 96
